2549ee01d590cf5f28b375be02ea614c9e7650c3
dqn <dqn270@gmail.com>
2026-02-10 00:42:42 +0900

    fix: suppress noisy chromiumoxide ERROR logs
    Add chromiumoxide=off tracing filter to silence harmless
    deserialization errors from unrecognized CDP events.

--- src/main.rs
+++ src/main.rs
@@ -23,7 +23,8 @@ async fn main() -> Result<()> {
     tracing_subscriber::fmt()
         .with_env_filter(
             tracing_subscriber::EnvFilter::from_default_env()
-                .add_directive(tracing::Level::INFO.into()),
+                .add_directive(tracing::Level::INFO.into())
+                .add_directive("chromiumoxide=off".parse().unwrap()),
         )
         .init();
 
